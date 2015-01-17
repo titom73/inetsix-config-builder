5f8cece1477af010769c4f515c72e8716b332c43
Thomas <tom@inetsix.net>
2015-01-17 12:15:53 +0100

    Version 1 with Arparse menu and no Exception control

--- configuration-builder.py
+++ configuration-builder.py
@@ -13,12 +13,12 @@ parser = argparse.ArgumentParser(description="Configuration Builder")
 parser.add_argument('-v','--verbose' ,help='Increase Verbosity',action="store_true")
 parser.add_argument('-y', '--yaml', help='Provides YAML file to fill Jinja2 template, default=dict.yml',default='dict.yml')
 parser.add_argument('-t','--template' ,help='template file, default=./template.j2',default='./template.j2')
-parser.add_argument('-t','--template' ,help='Base to construct filename, default=generated-conf-',default='generated-conf-')
+parser.add_argument('-o','--output' ,help='Base to construct filename, default=generated-conf-',default='generated-conf-')
 # Manage All options and construct array
 options = parser.parse_args()
 
 
-print 'Start configuration building'
+print 'Start configuration builder'
 # YAML file.
 with open(options.yaml) as fh:
     data = yaml.load(fh.read())
@@ -28,13 +28,11 @@ with open(options.template) as t_fh:
     t_format = t_fh.read()
 
 for device in data:
-	
-	print '  - Generate config for '+str(device['mxRef'])
-	template = Template(t_format)
-	#print(template.render(data))
-
-	confFile = open(options.base+str(device['mxRef'])+'.conf','w')
-	confFile.write(template.render(device))
-	confFile.close()
+    print '  - Generate config for '+str(device['mxRef'])
+    template = Template(t_format)
+    #print(template.render(data))
+    confFile = open(options.output+str(device['mxRef'])+'.conf','w')
+    confFile.write(template.render(device))
+    confFile.close()
 
 print 'End of Script'
