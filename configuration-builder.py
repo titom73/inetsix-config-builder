7612f3ad41d5e10000e5b53ffff7db319f683f39
TiTom <tom@inetsix.net>
2015-06-15 12:23:32 +0200

    Add option to catch YAML key from command line

--- configuration-builder.py
+++ configuration-builder.py
@@ -4,6 +4,7 @@ import os
 import argparse
 from glob import glob
 from jinja2 import Template
+from pprint import pprint
 
 def display_help():
     print ""
@@ -13,27 +14,32 @@ parser = argparse.ArgumentParser(description="Configuration Builder")
 parser.add_argument('-v','--verbose' ,help='Increase Verbosity',action="store_true")
 parser.add_argument('-y', '--yaml', help='Provides YAML file to fill Jinja2 template, default=dict.yml',default='dict.yml')
 parser.add_argument('-t','--template' ,help='template file, default=./template.j2',default='./template.j2')
-parser.add_argument('-o','--output' ,help='Base to construct filename, default=generated-conf-',default='generated-conf-')
-#parser.add_argument('-dir','--directory' ,help='Directory to save configurations',default='./ouput')
+parser.add_argument('-b','--base' ,help='Base to construct filename, default=generated-conf-',default='generated-conf-')
+parser.add_argument('-k','--key' ,help='Key used in YAML file, default=hostname',default='hostname')
 # Manage All options and construct array
 options = parser.parse_args()
 
 
-print 'Start configuration builder'
+print 'Start configuration building'
 # YAML file.
-with open(options.yaml) as fh:
-    data = yaml.load(fh.read())
+with open( options.yaml ) as fh:
+    data = yaml.load( fh.read() )
+
+pprint( data )
 
 # Jinja2 template file.
-with open(options.template) as t_fh:
+with open( options.template ) as t_fh:
     t_format = t_fh.read()
 
 for device in data:
-    print '  - Generate config for '+str(device['mxRef'])
-    template = Template(t_format)
-    #print(template.render(data))
-    confFile = open(options.output+str(device['mxRef'])+'.conf','w')
-    confFile.write(template.render(device))
-    confFile.close()
+	hostname = device[ options.key ]
+	print '  - Generate config for '+options.key
+	print ''
+	#pprint( device )
+	template = Template( t_format )
+
+	confFile = open( options.base + str(hostname) + '.conf','w')
+	confFile.write( template.render( device ) )
+	confFile.close()
 
 print 'End of Script'
