4c2b7631a19de1b522001442031795006213196d
Thomas Grimonet <tgrimonet@arista.com>
2019-05-17 14:44:13 +0200

    Refactor to allow easy install mechanism

---
+++ setup.py
@@ -0,0 +1,35 @@
+import shutil
+from setuptools import setup
+
+# Load list of requirements from req file
+with open('requirements.txt') as f:
+    REQUIRED_PACKAGES = f.read().splitlines()
+
+# Load description from README file
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+# Rename Scripts to sync with original name
+shutil.copyfile('bin/jinja-render.py', 'bin/jinja-render')
+
+setup(
+    name="cvp-tools-scripts",
+    version='0.2',
+    scripts=["bin/jinja-render"],
+    python_requires=">=2.7",
+    install_requires=REQUIRED_PACKAGES,
+    url="https://github.com/titom73/inetsix-config-builder",
+    license="BSD",
+    author="Thomas Grimonet",
+    author_email="tom@inetsix.net",
+    description="Tool to render JINJA2 templates",
+    long_description=LONG_DESCRIPTION,
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ]
+)
